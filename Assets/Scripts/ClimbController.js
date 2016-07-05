b17bbae008d0ac81fb65a790b91f2c3a4bc3ea49
Derek Traver <derektraver@Dereks-MacBook-Pro.local>
2016-07-05 17:42:46 -0400

    many fixes, added animation

--- Assets/Scripts/ClimbController.js
+++ Assets/Scripts/ClimbController.js
@@ -4,22 +4,22 @@ var climbRayOrigin : Transform;
 var climbLayerMask : LayerMask;
 var targetPos : Transform;
 
+var climbing : boolean = false;
+var inTransition : boolean = false;
+
+var animator : Animator;
+var rb : Rigidbody;
 var moveScripts : MonoBehaviour[];
 
-//store the initial local offset of the climb ray origin
-private var climbRayInitialPos : Vector3;
-
-private var rb : Rigidbody;
-
 function Start(){
-	rb = GetComponent.<Rigidbody>();
-	climbRayInitialPos = climbRayOrigin.transform.localPosition;
+	if(!animator) animator = GetComponent.<Animator>();
+	if(!rb) rb = GetComponent.<Rigidbody>();
 }
 
 
 function FixedUpdate () {
 
-	if(Input.GetButton("Fire1")){
+	if(Input.GetKey(KeyCode.P)){
 
 		//capture input
 		var horizontal = Input.GetAxis("Horizontal"); 
@@ -27,34 +27,64 @@ function FixedUpdate () {
 
 		if(horizontal != 0.0 || vertical != 0.0){
 
+			//correct for pivot offset. this was done by trial and error :)
+			horizontal *= 0.5;
+			if(vertical > 0.1)
+				vertical *= 2.0;
+			else
+				vertical *= 0.5;
+
+
 			//movement direction is on x and z axes
 			var moveDir = Vector3(horizontal, vertical, 0);
 
 			//move the climbRayOrigin around the player on the x-z axes
-			climbRayOrigin.transform.localPosition = moveDir;
+			climbRayOrigin.transform.localPosition =  moveDir;
 
-			//cast from climbRayOrigin to the front of the player to detect climbable surfaces
-			var hit : RaycastHit;
+			if(!inTransition){
 
+				//cast from climbRayOrigin to the front of the player to detect climbable surfaces
+				var hit : RaycastHit;
 
+				//check for adjacent wall to transfer
 
-			if(
-				Physics.Linecast(transform.position, climbRayOrigin.position, hit, climbLayerMask) ||
-				Physics.Linecast(climbRayOrigin.position, transform.position + transform.forward, hit, climbLayerMask)
-			){
-				for(var moveScript : MonoBehaviour in moveScripts){
-					moveScript.enabled = false;
+				if(Physics.Linecast(transform.position, climbRayOrigin.position, hit, climbLayerMask)){
+					rb.velocity = Vector3.zero;
+					targetPos.position = hit.point + (hit.normal * 0.5);
+					if(vertical < -0.1) targetPos.position.y -= 1.6;
+					targetPos.rotation = Quaternion.FromToRotation(targetPos.forward, -hit.normal) * targetPos.rotation;
+					if(Physics.Raycast(targetPos.position, targetPos.up, 1.6, climbLayerMask)){
+						Debug.DrawRay(targetPos.position, targetPos.up*1.6, Color.red);
+					}
+					else{
+						StartCoroutine(TransitionToTargetPos());
+					}
 				}
-				rb.useGravity = false;
-				var destination = hit.point + (hit.normal * 0.5);
-				targetPos.position = destination;
-				var targetDir = (destination - transform.position).normalized;
-				rb.velocity = targetDir;
-				var targetRot = Quaternion.FromToRotation(transform.forward, -hit.normal) * transform.rotation;
-				transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, Time.deltaTime);
-				//transform.forward = Vector3.Slerp(transform.forward, -hit.normal, Time.deltaTime);
+				else if(
+					Physics.Linecast(climbRayOrigin.position, transform.position + transform.forward, hit, climbLayerMask) 
+				){
+					rb.useGravity = false;
+					for(var moveScript : MonoBehaviour in moveScripts){
+						moveScript.enabled = false;
+					}
+					if(!animator.GetBool('Climbing')){
+						animator.SetBool('Climbing', true);
+					}
 
-			}
+					var destination = hit.point + (hit.normal * 0.5);
+					targetPos.position = destination;
+					var targetDir = (destination - transform.position).normalized;
+					var targetRot = Quaternion.FromToRotation(transform.forward, -hit.normal) * transform.rotation;
+					targetPos.rotation = targetRot;
+
+
+					rb.velocity = targetDir;
+					transform.rotation = Quaternion.Slerp(transform.rotation, targetRot, Time.deltaTime * 5.0);
+					//transform.forward = Vector3.Slerp(transform.forward, -hit.normal, Time.deltaTime);
+
+				}
+
+			} //eof if(!inTransition)
 
 		}
 		else{
@@ -62,6 +92,28 @@ function FixedUpdate () {
 		}
 
 	}
- 
 
+	if(Input.GetKeyUp(KeyCode.P)){
+		rb.useGravity = true;
+		transform.up = Vector3.up;
+		for(var moveScript : MonoBehaviour in moveScripts){
+			moveScript.enabled = true;
+		}
+	}
+
+}
+
+function TransitionToTargetPos(){
+	inTransition = true;
+	var i = 0.0;
+    var rate = 1.0/0.5; // 1.0 / duration
+    var startPos = transform.position;
+    var startRot = transform.rotation;
+    while (i < 1.0){
+        i += Time.deltaTime * rate;
+        transform.position = Vector3.Lerp(startPos, targetPos.position, i);
+        transform.rotation = Quaternion.Slerp(startRot, targetPos.rotation, i);
+        yield; 
+    }
+    inTransition = false;
 }
